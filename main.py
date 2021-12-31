9dc00cb92db7bf3a8eb79eba6f4dbdf500b3909b
Gabriel Santos <gabriel.santos@wildlifestudios.com>
2021-12-31 15:41:52 -0300

    Created basic functions and request module

---
+++ main.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from flask import Flask, jsonify
+from pandas.io.parsers import read_csv
+
+app = Flask(__name__)
+data = pd.read_csv("username-password-recovery-code.csv")
+
+# API FUNCTIONS
+@app.route("/")
+def homepage():
+    return "API Running"
+
+
+@app.route("/location")
+def location():
+    for row in data.iterrows():  # iterate over the rows
+        data_push = {"location_list": row["Location"]}
+        return jsonify(data_push)
+
+
+@app.route("/total_salary")
+def total_salary():
+    data_push = {"total_salary": int(data["Salary"].sum())}
+    return jsonify(data_push)
+
+
+# RUN THE API
+app.run()
